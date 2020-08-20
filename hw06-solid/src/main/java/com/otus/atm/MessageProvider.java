d17de386fad3091b294543538263dc552bda2c96
Artur Skorik <admskorik@gmail.com>
2020-08-21 05:33:53 +1000

    removed unnecessary interfaces

--- hw06-solid/src/main/java/com/otus/atm/MessageProvider.java
+++
@@ -1,7 +0,0 @@
-package com.otus.atm;
-
-public interface MessageProvider {
-
-    String getMessageShowBalance(long amount);
-
-}
--- hw06-solid/src/main/java/com/otus/atm/Speaker.java
+++
@@ -1,7 +0,0 @@
-package com.otus.atm;
-
-public interface Speaker {
-
-    void sayMessage(String message);
-
-}
